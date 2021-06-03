c5c3280724ed75fd3bb5d0de11a56d97ee1de4a0
Ashu <e841018@gmail.com>
2021-06-04 06:05:36 +0800

    [new] finished most functions, haven't debug yet

--- sbm.h
+++ sbm.h
@@ -12,26 +12,44 @@ const int M = 2; // number of substeps in a step
 // degree of parallelism
 const int Pb = 2; // number of blocks
 const int Nb = N / Pb; // the number of spins to be handled in a block
-const int Pr = 2; // row size of J submatrix
-const int Pc = 2; // column size of J submatrix
 
-// These parameters are set according to p.6 of [2].
-// The corresponding components in [1] are annotated in comments.
-// [1] Combinatorial optimization by simulating adiabatic bifurcations in nonlinear Hamiltonian systems
-// [2] FPGA-based Simulated Bifurcation Machine
+/*
+These parameters are set according to p.6 of [2].
+The corresponding components in [1] are annotated in comments.
+[1] Combinatorial optimization by simulating adiabatic bifurcations in nonlinear Hamiltonian systems
+[2] FPGA-based Simulated Bifurcation Machine
+*/
 const float alpha0 = 1.; // Delta in [1]
 const float beta0 = 1.; // K in [1]
 const float std_of_J = 1.; // mentioned in [1], haven't calculate yet
 const float gamma0 = 0.7 * alpha0 / std_of_J / n; // Xi0 in [1]
 const float Delta_t = 0.9;
+const float Delta_t_times_gamma0 = Delta_t * gamma0;
 const float delta_t = Delta_t / M;
 const float alpha_init = 0.; // p(t) in [1], changes linearly from 0 to 1
 const float Delta_alpha = (1. - alpha_init) / N_step;
 
-void qubo2ising(float xrate[n][n], int J[N][N], float h[N]);
+const float M1 = 1.;
+const float M2 = 1.;
+
+void top(float xrate[n][n], bool activation[n][n]);
+void QUBO2Ising(float xrate[n][n], int J[N][N], float h[N]);
 void SBM(int J[N][N], float h[N], bool spin[N]);
-void MMTE(float x_prime_in[N], float x_prime_out[Nb]);
-void MM(int Ji[Nb][N], float x[N], float Delta_P[Nb]);
-void TE(float x_in[Nb], float p_in[Nb], float x_out[Nb], float p_out[Nb]);
+void MMTE(int step, int J_sub[Nb][N], float h_sub[Nb], float x_prime_in[N], float x_prime_out[Nb]);
+void MM(int step, int J_sub[Nb][N], float x_prime_in[N], float Delta_P[Nb]);
+void TE(int step, float h_sub[Nb], float x_sub[Nb], float p_sub[Nb]);
+void rand_init(float arr[Nb]);
 
 #endif
+
+/*
+TODOs:
+ * rand_init FPGA implementation
+ * calculate std_of_J
+ * TE
+Improvements:
+ * generate J[N][N] at compile time
+ * is double buffer implemented correctly?
+ * change J[N][N] to a custom datatype using less bits
+ * wrap in a class?
+*/
--- sbm_Ma.cpp
+++ sbm_Ma.cpp
@@ -1,1 +1,5 @@
 # include "sbm.h"
+
+// maybe you can use these?
+float alpha = alpha_init;
+float eta = alpha; // stated to be dependent on alpha(t) in [2], not presents in [1]
